84cf2ddf4076c4a72c02b745e88c71a3ced0bc0f
uns0sh1 <unsoshi.sdt@gmail.com>
2023-07-10 12:22:06 +0700

    Pt_3_5: List of non prime numbers
    Program asks user to input two numbers and outputs all the non prime
    numbers between the given two using list comprehensions

---
+++ Pt_3_5.py
@@ -0,0 +1,14 @@
+def isprime(n):
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
+
+
+a = int(input("Введите левую границу списка: "))
+b = int(input("Введите правую границу списка: "))
+if a > b:
+    a, b = b, a
+print([x for x in range(a, b + 1) if not(isprime(x))])
