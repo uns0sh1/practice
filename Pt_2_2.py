60d4f8e18f78e49fdd49779e2c74bb3279664505
uns0sh1 <unsoshi.sdt@gmail.com>
2023-07-10 08:43:47 +0700

    Pt_2_2: Adding TV program to the list
    Program outputs the list of 4 TV programs and asks user to input new TV
    program to certain position in the list. The final list of 5 TV
    programs should be outputted

---
+++ Pt_2_2.py
@@ -0,0 +1,10 @@
+shows = ["Импровизация", "Кухня", "След", "Касл"]
+print("Список передач: ")
+for i in shows:
+    print(i)
+newShow = input("Введите название новой передачи: ")
+position = int(input("Введите позицию передачи (от 1 до 4): "))
+shows.insert(position-1, newShow)
+print("Измененный список передач: ")
+for i in shows:
+    print(i)
