a10eadcee92870d64d1da4846734f886c69f3fef
uns0sh1 <unsoshi.sdt@gmail.com>
2023-07-12 19:42:54 +0700

    Pt_5_2: Adding new books. Search by author
    Using the previous made file, program asks user how many new books they
    want to add to the list and asks user to input data for a new books.
    After that, program asks user to input an author, whose books they want
    to search in the list. If there are no books of user's author, relevant
    message will be outputted

---
+++ Pt_5_2.py
@@ -0,0 +1,25 @@
+import csv
+
+filename = "favourite_books.csv"
+amount = int(input("Сколько записей вы хотите добавить в список?\n"))
+for i in range(amount):
+    print("Запись №" + str(i+1))
+    book = input("Введите название книги: ")
+    author = input("Введите автора книги: ")
+    year = input("Введите год выпуска книги: ")
+    row = [book, author, year]
+    with open(filename, "a", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
+    print("Запись добавлена!")
+
+name = input("Введите автора для поиска его книг в списке: ")
+with open(filename, "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    flag = False
+    for row in reader:
+        if name in row["Author"]:
+            flag = True
+            print(row)
+    if not flag:
+        print("Не было найдено ни одной книги указанного вами автора")
