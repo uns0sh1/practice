999f458485a0c1f19d9d3794e701be4150b921e2
uns0sh1 <unsoshi.sdt@gmail.com>
2023-07-12 18:21:47 +0700

    Pt_4_4: Combs of numbers equal to the given number
    Program asks user to input a length of the list of random numbers
    from 1 to 10 and a target number. Program outputs all the combinations
    of numbers from the list, sum of which gives the result equal to a
    target number

---
+++ Pt_4_4.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+from random import randint
+
+length = int(input("Введите длину списка чисел: "))
+numlist = [randint(1, 10) for x in range(length)]
+print("Список чисел:", numlist)
+
+num = int(input("Введите число: "))
+comb = []
+res = []
+
+if length > 0:
+    for i in range(0, length+1):
+        comb += list(combinations(numlist, i))
+    for value in comb:
+        if sum(value) == num:
+            res.append(value)
+resfinal = list(set(res))
+print("Уникальные комбинации чисел, сумма которых равна " + str(num) + ": " + str(resfinal))
