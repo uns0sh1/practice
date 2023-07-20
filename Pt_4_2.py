130d735a8db6d9fe78978c859293ec2a32a3a0e3
uns0sh1 <unsoshi.sdt@gmail.com>
2023-07-20 18:53:04 +0700

    Pt_4_2: Search for palindromes in a string
    Program asks user to input a string and outputs all the substrings of a
    given string which are palindromes
    UPD: Added whitespaces around the ariphmetic operators and length check
    of found palindromes

--- Pt_4_2.py
+++ Pt_4_2.py
@@ -1,5 +1,5 @@
 def palindrome(n):
-    if n == n[::-1]:
+    if n == n[::-1] and len(n) != 1:
         return True
     return False
 
@@ -8,8 +8,11 @@ array = input("Введите строку: ")
 res = set()
 
 for i in range(len(array)):
-    for j in range(i+1, len(array)+1):
+    for j in range(i + 1, len(array) + 1):
         if palindrome(array[i:j]):
             res.add(array[i:j])
 
-print("Найденные палиндромы:", res)
+if len(res) == 0:
+    print("Не найдено ни одного палинидрома")
+else:
+    print("Найденные палиндромы:", res)
